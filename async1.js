360a17afae5b29773f74cf4e9071a70ed74a312b
ashish-kattamuri <69315739+ashish-kattamuri@users.noreply.github.com>
2022-02-18 11:04:10 +0530

    Add files via upload

---
+++ async1.js
@@ -0,0 +1,21 @@
+
+const printName = (name) => (console.log("Hi" + name));
+
+
+    const printBill = function(name, bill){
+        return "Hi " + name + ", please pay: " + bill;}
+
+
+const person = {
+
+    name: "oam Chomsky",
+    age: 92
+
+    
+}
+
+let nam = person.name;
+let age = person.age;
+
+console.log(nam);
+console.log(age);
